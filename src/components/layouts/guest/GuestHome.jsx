c573053884a8824a8a499273adb5ea41ee20596d
JCWoodworker <jc@rilocalwoodworks.com>
2023-03-05 23:59:44 -0500

    changed title

--- src/components/layouts/guest/GuestHome.jsx
+++ src/components/layouts/guest/GuestHome.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Image } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 import FadeInSection from "../../services/FadeInSection"
 
 const GuestHome = () => {
@@ -8,7 +8,7 @@ const GuestHome = () => {
 			<Container>
 				<Row className="project-row">
 					<h1>
-						Welcome to The Inventory App!
+						Welcome to The Home  Inventory App!
 					</h1>
 					<h3>
 						Use this app to manage all the storage within your home or business.  You can add locations, rooms, items, and even add photos to each item.  This app is a work in progress, so please be patient as I continue to add features!
